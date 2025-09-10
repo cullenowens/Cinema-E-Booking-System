fc100508976a26d8aebfc5745da56e340556ca08
Zach Locker <zacharywlocker@gmail.com>
2025-09-10 11:14:37 -0400

    Add Zach Locker to name doc

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-#connecting with Github
-#use "#" and put your name and commit your work when you're
-#able to access this file
+# connecting with Github
+# use "#" and put your name and commit your work when you're
+# able to access this file
+
+# Zach Locker
+
